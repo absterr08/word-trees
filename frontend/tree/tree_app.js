c589a4da037a2520907a73ee3615fcf4955dddb1
Abigail <abbychersh@gmail.com>
2020-04-27 09:13:41 -0400

    Remove list button

--- frontend/tree/tree_app.js
+++ frontend/tree/tree_app.js
@@ -3,12 +3,7 @@ import drawTree from './tree.js';
 
 export default function(root) {
     const input = document.getElementById('word-input');
-
-    const getListButton = document.getElementById('list');
-    getListButton.addEventListener('click', () => {
-        getSynonymList(input.value, res => console.log(res)); 
-    });
-
+    
     const getTreeButton = document.getElementById('tree');
     getTreeButton.addEventListener('click', () => {
         getSynonymTree(input.value, res => drawTree(res));
