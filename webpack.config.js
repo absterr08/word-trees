eccf0a1ff349f31508d49bd26eded203aad84ceb
Abigail <abbychersh@gmail.com>
2020-04-27 01:45:48 -0400

    Add procfile

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
     entry: path.join(__dirname, "frontend", "index.jsx"),
     output: {
-        path: path.join(__dirname, "flaskr", "static"),
+        path: path.join(__dirname, "app", "static"),
         filename: "bundle.js"
     }
 };
